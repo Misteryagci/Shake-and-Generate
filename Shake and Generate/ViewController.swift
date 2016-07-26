7f8b0e058f9ae611af80d1f4b98747f5fc90fe1e
Kaan Yagci <kaanyagci@pc5.home>
2016-07-27 00:42:04 +0200

    Initial Commit

---
+++ Shake and Generate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shake and Generate
+//
+//  Created by Kaan Yagci on 27/07/2016.
+//  Copyright © 2016 Androctone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Shake and GenerateTests/Shake_and_GenerateTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shake_and_GenerateTests.swift
+//  Shake and GenerateTests
+//
+//  Created by Kaan Yagci on 27/07/2016.
+//  Copyright © 2016 Androctone. All rights reserved.
+//
+
+import XCTest
+@testable import Shake_and_Generate
+
+class Shake_and_GenerateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
